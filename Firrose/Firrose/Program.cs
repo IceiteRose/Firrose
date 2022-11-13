b78e5b18f08e47e60b64a6593a49992619e100c3
IceiteRose <118106465+IceiteRose@users.noreply.github.com>
2022-11-13 15:07:19 +0200

    Add files via upload

---
+++ Firrose/Firrose/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Firrose
+{
+
+    static class Program
+    {
+        static char commandsKey;
+        static char inputModeCommand;
+        static string currentMode = "console";
+
+        public static void ConsoleMode()
+        {
+            commandsKey = Convert.ToChar(Console.Read());
+            inputModeCommand = Convert.ToChar(Console.Read());
+            if (commandsKey == ':' && inputModeCommand == ' ')
+            {
+                currentMode = "console";
+            }
+        }
+        static void InputMode()
+        {
+            commandsKey = Convert.ToChar(Console.Read());
+            inputModeCommand = Convert.ToChar(Console.Read());
+            if (commandsKey == ':' && inputModeCommand == 'i')
+            {
+                currentMode = "input";
+            }
+        }
+        
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(currentMode);
+
+            InputMode();
+            ConsoleMode();
+
+            while (currentMode == "console")
+            {
+
+            }
+            
+            while (currentMode == "input")
+            {
+
+            }
+
+            Console.WriteLine(currentMode);
+        }
+    }
+}
